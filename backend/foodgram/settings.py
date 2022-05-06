8157eabfd0bd8e7cdc093348d128a4e25e7b4c99
Vladislavhgtech <you@example.com>
2022-05-06 17:28:17 +0300

    Настройка DockerHub

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,8 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
