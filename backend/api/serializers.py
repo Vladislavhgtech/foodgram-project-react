c4e63f01337474b1ec3cdefe7b06858ebe615ac1
Vladislavhgtech <you@example.com>
2022-05-07 16:53:21 +0300

    Корректировка serializer.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -132,9 +132,9 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
                 ingredient_id=id,
                 recipe=recipe,
                 amount=amount))
-        IngredientAmount.objects.bulk_create(entity_list)   
+        IngredientAmount.objects.bulk_create(entity_list)
         return recipe
-
+    
     @transaction.atomic
     def update(self, instance, validated_data):
         instance.ingredients.clear()
@@ -144,7 +144,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         tags = self.initial_data.get('tags')
         for tag in tags:
             instance.tags.add(tag)
-        
+
         entity_list = []
         for ingredient in ingredients_data:
             ingredient_id = ingredient.get('id')
